3c19fe51d7b64cbda3ba83b073223cf745e8469d
lvyahui <lvyahui8@gmail.com>
2023-09-02 17:27:47 +0800

    test

--- src/main/java/org/example/ASMifierTest.java
+++ src/main/java/org/example/ASMifierTest.java
@@ -2,9 +2,14 @@ package org.example;
 
 import org.objectweb.asm.util.ASMifier;
 
+import java.lang.reflect.Field;
+
 public class ASMifierTest {
     public static void main(String[] args) throws Exception {
         // 输出应该用什么样的代码来生成java.lang.Runnable
         ASMifier.main(new String[]{"java.lang.Runnable"});
+        Field field = ASMifier.class.getDeclaredField("USAGE");
+        field.setAccessible(true);
+        System.out.println(field.get(null));
     }
 }
